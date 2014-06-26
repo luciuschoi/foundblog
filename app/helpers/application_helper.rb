40bc603afe10e9a721cf1a643873a7f98ce6abf1
Hyo Seong Choi <rorlab@gmail.com>
2014-06-26 10:51:38 +0900

    제3장 다이나믹 레이아웃의 작성

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "success"   # Green
+      when "error"
+        "danger"    # Red
+      when "alert"
+        "alert"   # Yellow
+      when "notice"
+        "info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
+  def active_menu(target_controller)
+    "class=#{controller_name == target_controller ? 'active' : ''}"
+  end
+
+  def user_email(post)
+    post.user.present? ? post.user.email : "an anonymous user"
+  end
+
+  def user_roles(user)
+    user.roles.map(&:name).join(', ').titleize
+  end
 end
